bacb834fa7a69d98eef70685f488f338a7eb5acc
Ryan Gabriel <rlksx@pop-os.localdomain>
2022-11-09 22:12:21 -0300

    add: Accounts

---
+++ Account.cs
@@ -0,0 +1,15 @@
+namespace console_login_system
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        public Account()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
+}
