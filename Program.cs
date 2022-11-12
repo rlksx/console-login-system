9608f1601e911722c80fd7547537c694f80a0236
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-12 00:48:19 -0300

    add: signin-signup-option

--- Program.cs
+++ Program.cs
@@ -7,24 +7,58 @@ namespace console_login_system
         {
             Console.Clear();
 
-            try
-            {
-                Console.SetWindowSize(130, 34);
-            }
-            catch (System.Exception)
-            {
-                isSignInOrSignUp();
-            }
+            try { Console.SetWindowSize(130, 34); } 
+            catch (System.Exception) { checkWindowSize(); }
 
             var signInAccount = new SignInAccount();
             var signUpNewAccount = new SignUpNewAccount();
-            // signInAccount.start();
-            signUpNewAccount.start();
+            Console.CursorVisible = true;
+
+            Thread.Sleep(800);
+            bool isSignIn = isSignInOrSignUp();
+            switch(isSignIn)
+            {
+                case true: signInAccount.start(); break;
+                case false: signUpNewAccount.start(); break;
+            }
             
             Console.ReadKey(); 
         }
 
-        public static void isSignInOrSignUp ()
+    private static bool isSignInOrSignUp()
+    {
+        int windowHight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
+
+        bool isSignIn = true;
+        
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| Ja possui uma conta conosco? (S/N) :    |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Console.SetCursorPosition(windowWidth/2 + 16, windowHight/2);
+        var response = Console.ReadLine();
+
+        switch (response.ToLower().Trim())
+        {
+            case "s": isSignIn = true; break;
+            case "n": isSignIn = false; break;
+            default: isSignInOrSignUp(); break;
+        }
+
+
+      return isSignIn;
+    }
+
+    private static void checkWindowSize()
         {
             Console.CursorVisible = false;
             int currentWindowHight = Console.WindowHeight;
@@ -47,7 +81,7 @@ namespace console_login_system
                 Console.SetCursorPosition(currentWindowWidth/2 - $"Width = 100 Height = 100".Length/2, currentWindowHight/2 + 1);
                 Console.Write("Width = 130 Height = 33");
 
-                Thread.Sleep(100);
+                Thread.Sleep(60);
                 Console.Clear();
             }
         }
