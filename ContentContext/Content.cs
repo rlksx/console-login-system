c428946f3f6d7d3ce8a7fa4ccde68c2e024e4705
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-15 18:18:11 -0300

    add: changePassword-changeUserName

--- ContentContext/Content.cs
+++ ContentContext/Content.cs
@@ -37,46 +37,33 @@ namespace console_login_system.ContentContext
 
         Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
         Console.Write(@"| 4 - View your user infomation 0.0       |");
-        
+
         Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 2);
+        Console.Write(@"| 5 - Exit                                |");
+        
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 3);
         Console.Write(@"|  -------------------------------------  |");
 
-        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 3);
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 4);
         Console.Write(@"| R :                                     |");
         
-        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 4);
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 5);
         Console.Write(@"\_________________________________________/");
 
-        Console.SetCursorPosition(windowWidth/2 - 17, windowHight/2 + 3);
-        int answer = Convert.ToInt32(Console.ReadLine());
+        Console.SetCursorPosition(windowWidth/2 - 17, windowHight/2 + 4);
+        string answer = Console.ReadLine();
 
+        var manipulateInformation = new ManipulateInformation();
         switch(answer)
         {
-          case 1: changeUser(); break;
-          case 2: changePassword(); break;
-          case 3: deleteAccount(); break;
-          case 4: viewInformation(); break;
-        }
-    }
-
-    private void viewInformation()
-    {
-      throw new NotImplementedException();
-    }
-
-    private void deleteAccount()
-    {
-      throw new NotImplementedException();
-    }
-
-    private void changePassword()
-    {
-      throw new NotImplementedException();
-    }
-
-    private void changeUser()
-    {
-      throw new NotImplementedException();
+          case "1": manipulateInformation.changeUser(IdUser); break;
+          case "2": manipulateInformation.changePassword(IdUser); break;
+          case "3": manipulateInformation.deleteAccount(IdUser); break;
+          case "4": manipulateInformation.viewInformation(IdUser); break;
+          case "5": Console.Clear(); Thread.Sleep(800); Environment.Exit(1); break;
+          default: drawMenu(); break;
+        
+      } 
     }
   }
 }
--- ManipulateInformations.cs
+++ ManipulateInformations.cs
@@ -1,7 +1,121 @@
+using Dapper;
+using Microsoft.Data.SqlClient;
 namespace console_login_system
 {
   class ManipulateInformation
   {
-    
+    public string stringConnection { get; set; }
+    SignInAccount signInAccount = new SignInAccount();
+
+    public ManipulateInformation()
+    {
+      const string stringConnection = "Server=localhost,1433;Database=ConsoleLoginData;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;";
+      this.stringConnection = stringConnection;
+    }
+
+    public void changeUser(Guid id)
+    {
+      Console.Clear();
+        int windowHight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
+
+        
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| New username :                          |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Console.SetCursorPosition(windowWidth/2 - 6, windowHight/2);
+        var newUserName = Console.ReadLine();
+
+      using(var connection = new SqlConnection(stringConnection))
+      {
+        var sql = @"
+          UPDATE [Account] SET [User] = @newUserName WHERE [Id] = @id
+        ";
+
+        connection.Open();
+        connection.Execute(sql, new { newUserName, id } );
+      }
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| username has been successfully changed! |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Thread.Sleep(1200);
+        signInAccount.start();
+    }
+    public void changePassword(Guid id)
+    {
+       Console.Clear();
+        int windowHight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
+
+        
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| New password :                          |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Console.SetCursorPosition(windowWidth/2 - 6, windowHight/2);
+        var newPassword = Console.ReadLine();
+
+      using(var connection = new SqlConnection(stringConnection))
+      {
+        var sql = @"
+          UPDATE [Account] SET [Password] = @newPassword WHERE [Id] = @id
+        ";
+
+        connection.Open();
+        connection.Execute(sql, new { newPassword, id } );
+      }
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| Password has been successfully changed! |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Thread.Sleep(1200);
+        signInAccount.start();
+    }
+
+    public void deleteAccount(Guid id)
+    {
+
+    }
+
+    public void viewInformation(Guid id)
+    {
+
+    }
   }
 }
