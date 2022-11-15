5f51588926bd649537ce194b529afb8f6b2aeaaa
Ryan Gabriel <ryan.bromati@hotmail.com>
2022-11-15 18:52:16 -0300

    add: deleteAccount-viewInformation

--- ManipulateInformations.cs
+++ ManipulateInformations.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Dapper;
 using Microsoft.Data.SqlClient;
 namespace console_login_system
@@ -110,12 +111,97 @@ namespace console_login_system
 
     public void deleteAccount(Guid id)
     {
+        Console.Clear();
+        int windowHight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
 
+        
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"| Are you sure? (S/N) :                   |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Console.SetCursorPosition(windowWidth/2 + 1, windowHight/2);
+        var respose = Console.ReadLine();
+        if(respose.ToLower() == "n") Thread.Sleep(800); signInAccount.start();
+
+      using(var connection = new SqlConnection(stringConnection))
+      {
+        var sql = @"
+          DELETE [Account] WHERE [Id] = @id
+        ";
+
+        connection.Open();
+        connection.Execute(sql, new { id } );
+      }
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        Console.Write(@" _________________________________________");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        Console.Write(@"/                                         \");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        Console.Write(@"|  Account has been successfully deleted  |");
+
+        Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        Console.Write(@"\_________________________________________/");
+
+        Thread.Sleep(1200);
+        Environment.Exit(1);
     }
 
     public void viewInformation(Guid id)
     {
+        Console.Clear();
+        int windowHight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
 
+      using(var connection = new SqlConnection(stringConnection))
+      {
+        var sql = @"
+          SELECT * FROM [Account] WHERE [Id] = @id
+        ";
+
+        connection.Open();
+        var userInformation = connection.Query(sql, new { id } );
+
+        // Console.SetCursorPosition(windowWidth/2 - 10, windowHight/2);
+        foreach (var item in userInformation)
+        {
+          Console.WriteLine("Email : " + item.Email);
+          Console.WriteLine("User : " + item.User);
+          Console.WriteLine("Password : " + item.Password);
+        }
+      }
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 2);
+        // Console.Write(@" _________________________________________");
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 - 1);
+        // Console.Write(@"/                                         \");
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2);
+        // Console.Write(@"| Email :                                 |");
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 1);
+        // Console.Write(@"| Username:                               |");
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 2);
+        // Console.Write(@"| Password :                              |");
+
+        // Console.SetCursorPosition(windowWidth/2 - 23, windowHight/2 + 3);
+        // Console.Write(@"\_________________________________________/");
+
+        Thread.Sleep(1200);
+        Environment.Exit(1);
     }
   }
 }
